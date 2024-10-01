fc50c8b647167befc3783321cee53e1b025463be
laurpritch07 <156968646+laurpritch07@users.noreply.github.com>
2024-10-01 17:05:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ for (i = 0; i < acc.length; i++) {
 
         /*toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
+        if (panel.style.maxHeight === "block") {
+            panel.style.maxHeight = "null";
         } else {
-            panel.style.display = "block";
+            panel.style.maxHeight = panel.scrollHeight + "px";
         }
     });
 }
