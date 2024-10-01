328a45e45aecab116664d537d4ad62261cfd39dd
laurpritch07 <156968646+laurpritch07@users.noreply.github.com>
2024-10-01 17:13:13 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        /*toggle between adding and removing the "active" class*/
-        this.classList.toggle("active");
-
+      this.classList.toggle("active");
         /*toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
