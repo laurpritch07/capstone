2139ba4a16f677d0cd322f787a4d6f1b162ff3bf
laurpritch07 <156968646+laurpritch07@users.noreply.github.com>
2024-10-06 15:20:52 -0500

    Homepage updates, navigation css

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
       this.classList.toggle("active");
+      this.textContent = 'Learn More';
         /*toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
           panel.style.maxHeight = null;
         } else {
           panel.style.maxHeight = panel.scrollHeight + "px";
+          this.textContent = 'Hide';
         }
       });
     }
+
+
